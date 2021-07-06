aa5651f893ec4a960cca038c49a4c933e7685bf0
OBess <dima.suschenko03@gmail.com>
2021-07-06 14:32:54 +0300

    Added methods 'add', 'get', 'operator()' and tests fot them.

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,8 +1,63 @@
 #pragma once
 
-class graph
+// C++
+#include <iterator>
+#include <fstream>
+#include <vector>
+#include <map>
+
+class or_graph
 {
 public:
+   using value = std::map<int, unsigned>;
+   using iterator = value::iterator;
+   using result = unsigned;
+   using reference_result = result &;
+
+   // Constructors
+   or_graph() noexcept {};
+   or_graph(const or_graph &other) noexcept;
+   or_graph(or_graph &&other) noexcept;
+
+   // Logic
+   void add(int a, int b, unsigned weight) noexcept;
+
+   // Getters
+   reference_result get(int a, int b) noexcept;
+   reference_result operator()(int a, int b) noexcept;
+
+   // Assignments operators
+   or_graph &operator=(const or_graph &other) noexcept;
+
+   or_graph &operator=(or_graph &&other) noexcept;
+
+   // Method for copy-and-swap idiom
+   friend void swap(or_graph &a, or_graph &b) noexcept;
 
 private:
+   std::map<int, value> m_nodes;
 };
+
+// Main method that read dataset  from file and return them.
+static or_graph make_or_graph(const std::string &path)
+{
+   // Our file
+   std::ifstream file(path);
+
+   // Oject to return
+   or_graph gr;
+
+   // Tmp values
+   int a, b;
+   unsigned weight;
+
+   // Read dataset from file and store it
+   while (file >> a)
+   {
+      file >> b;
+      file >> weight;
+      gr.add(a, b, weight);
+   }
+
+   return gr;
+}
--- tests.cpp
+++ tests.cpp
@@ -9,11 +9,11 @@
 #include "include/graph.hpp"
 
 // Commin path to file with or_graph for testing
-static constexpr std::string path("or_graph.txt");
+static const std::string path("graph.txt");
 
 // ---------------------- TESTS
 
-TEST(or_graph, ReadData)
+TEST(oriented_graph, ReadData)
 {
    or_graph gr = make_or_graph(path);
 
@@ -22,6 +22,22 @@ TEST(or_graph, ReadData)
    ASSERT_EQ(gr(0, 2), 2);
    ASSERT_EQ(gr(2, 3), 5);
    ASSERT_EQ(gr(2, 4), 7);
+}
+
+TEST(or_graph, AddNewNodeAndConnection)
+{
+   or_graph gr = make_or_graph(path);
+
+   gr.add(1, 3, 5);
+   gr.add(5, 1, 2);
+
+   ASSERT_EQ(gr(0, 1), 3);
+   ASSERT_EQ(gr(1, 0), 4);
+   ASSERT_EQ(gr(0, 2), 2);
+   ASSERT_EQ(gr(2, 3), 5);
+   ASSERT_EQ(gr(2, 4), 7);
+   ASSERT_EQ(gr(1, 3), 5);
+   ASSERT_EQ(gr(5, 1), 2);
 }
 
 TEST(or_graph, GetNode)
@@ -29,113 +45,100 @@ TEST(or_graph, GetNode)
    or_graph gr = make_or_graph(path);
 
    ASSERT_EQ(gr(0, 1), 3);
-
    gr(0, 1) = 4;
-
    ASSERT_EQ(gr(0, 1), 4);
-}
 
-TEST(or_graph, AddNewNodeOrConnection)
-{
-   or_graph gr = make_or_graph(path);
-
-   gr.add(1, 3, 5);
-   gr.add(5, 1, 2);
-
-   ASSERT_EQ(gr(0, 1), 3);
-   ASSERT_EQ(gr(1, 0), 4);
-   ASSERT_EQ(gr(0, 2), 2);
-   ASSERT_EQ(gr(2, 3), 5);
    ASSERT_EQ(gr(2, 4), 7);
-   ASSERT_EQ(gr(1, 3), 5);
-   ASSERT_EQ(gr(1, 5), 7);
+   gr.get(2, 4) = 5;
+   ASSERT_EQ(gr(2, 4), 5);
+
 }
 
-TEST(or_graph, RemoveNodeOrConnection)
-{
-   or_graph gr = make_or_graph(path);
+// TEST(or_graph, RemoveNodeOrConnection)
+// {
+//    or_graph gr = make_or_graph(path);
 
-   gr.add(1, 3, 5);
-   gr.add(5, 1, 2);
+//    gr.add(1, 3, 5);
+//    gr.add(5, 1, 2);
 
-   ASSERT_EQ(gr(0, 1), 3);
-   ASSERT_EQ(gr(1, 0), 4);
-   ASSERT_EQ(gr(0, 2), 2);
-   ASSERT_EQ(gr(2, 3), 5);
-   ASSERT_EQ(gr(2, 4), 7);
-   ASSERT_EQ(gr(1, 3), 5);
-   ASSERT_EQ(gr(2, 0), 7);
+//    ASSERT_EQ(gr(0, 1), 3);
+//    ASSERT_EQ(gr(1, 0), 4);
+//    ASSERT_EQ(gr(0, 2), 2);
+//    ASSERT_EQ(gr(2, 3), 5);
+//    ASSERT_EQ(gr(2, 4), 7);
+//    ASSERT_EQ(gr(1, 3), 5);
+//    ASSERT_EQ(gr(2, 0), 7);
 
-   gr.remove(1, 0);
-   gr.remove(2);
+//    gr.remove(1, 0);
+//    gr.remove(2);
 
-   ASSERT_EQ(gr(0, 1), 3);
-   ASSERT_EQ(gr(1, 0), -1);
-   ASSERT_EQ(gr(0, 2), 2);
-   ASSERT_EQ(gr(2, 3), -1);
-   ASSERT_EQ(gr(2, 4), -1);
-   ASSERT_EQ(gr(1, 3), 5);
-   ASSERT_EQ(gr(2, 0), -1);
-}
+//    ASSERT_EQ(gr(0, 1), 3);
+//    ASSERT_EQ(gr(1, 0), -1);
+//    ASSERT_EQ(gr(0, 2), 2);
+//    ASSERT_EQ(gr(2, 3), -1);
+//    ASSERT_EQ(gr(2, 4), -1);
+//    ASSERT_EQ(gr(1, 3), 5);
+//    ASSERT_EQ(gr(2, 0), -1);
+// }
 
-TEST(or_graph, CopyConstructor)
-{
-   or_graph gr = make_or_graph(path);
+// TEST(or_graph, CopyConstructor)
+// {
+//    or_graph gr = make_or_graph(path);
 
-   or_graph gr1(gr);
-   ASSERT_EQ(gr1(0, 1), 3);
-   ASSERT_EQ(gr1(1, 0), 4);
-   ASSERT_EQ(gr1(0, 2), 2);
-   ASSERT_EQ(gr1(2, 3), 5);
-   ASSERT_EQ(gr1(2, 4), 7);
+//    or_graph gr1(gr);
+//    ASSERT_EQ(gr1(0, 1), 3);
+//    ASSERT_EQ(gr1(1, 0), 4);
+//    ASSERT_EQ(gr1(0, 2), 2);
+//    ASSERT_EQ(gr1(2, 3), 5);
+//    ASSERT_EQ(gr1(2, 4), 7);
 
-   or_graph g2 = gr1;
-   ASSERT_EQ(gr2(0, 1), 3);
-   ASSERT_EQ(gr2(1, 0), 4);
-   ASSERT_EQ(gr2(0, 2), 2);
-   ASSERT_EQ(gr2(2, 3), 5);
-   ASSERT_EQ(gr2(2, 4), 7);
-}
+//    or_graph g2 = gr1;
+//    ASSERT_EQ(gr2(0, 1), 3);
+//    ASSERT_EQ(gr2(1, 0), 4);
+//    ASSERT_EQ(gr2(0, 2), 2);
+//    ASSERT_EQ(gr2(2, 3), 5);
+//    ASSERT_EQ(gr2(2, 4), 7);
+// }
 
-TEST(or_graph, BFS)
-{
-   or_graph gr = make_or_graph(path);
+// TEST(or_graph, BFS)
+// {
+//    or_graph gr = make_or_graph(path);
 
-   std::vector<int> v{0, 1, 2, 3, 4};
-   auto r = gr.bfs(0);
-   for (size_t i = 0; i < v.size(); ++i)
-      ASSER_EQ(v[i], r[i]);
+//    std::vector<int> v{0, 1, 2, 3, 4};
+//    auto r = gr.bfs(0);
+//    for (size_t i = 0; i < v.size(); ++i)
+//       ASSER_EQ(v[i], r[i]);
 
-   std::vector<int> v1{2, 3, 4};
-   auto r1 = gr.bfs(2);
-   for (size_t i = 0; i < v1.size(); ++i)
-      ASSER_EQ(v1[i], r1[i]);
-}
+//    std::vector<int> v1{2, 3, 4};
+//    auto r1 = gr.bfs(2);
+//    for (size_t i = 0; i < v1.size(); ++i)
+//       ASSER_EQ(v1[i], r1[i]);
+// }
 
-TEST(or_graph, DFS)
-{
-   or_graph gr = make_or_graph(path);
+// TEST(or_graph, DFS)
+// {
+//    or_graph gr = make_or_graph(path);
 
-   std::vector<int> v{0, 1, 2, 3, 4};
-   auto r = gr.dfs(0);
-   for (size_t i = 0; i < v.size(); ++i)
-      ASSER_EQ(v[i], r[i]);
+//    std::vector<int> v{0, 1, 2, 3, 4};
+//    auto r = gr.dfs(0);
+//    for (size_t i = 0; i < v.size(); ++i)
+//       ASSER_EQ(v[i], r[i]);
 
-   std::vector<int> v1{2, 3, 4};
-   auto r1 = gr.dfs(2);
-   for (size_t i = 0; i < v1.size(); ++i)
-      ASSER_EQ(v1[i], r1[i]);
-}
+//    std::vector<int> v1{2, 3, 4};
+//    auto r1 = gr.dfs(2);
+//    for (size_t i = 0; i < v1.size(); ++i)
+//       ASSER_EQ(v1[i], r1[i]);
+// }
 
-TEST(or_graph, Dijkstra)
-{
-   or_graph gr = make_or_graph(path);
+// TEST(or_graph, Dijkstra)
+// {
+//    or_graph gr = make_or_graph(path);
 
-   std::vector<int> v{0, 2, 3, 4};
-   auto r = gr.shortest(0, 4);
-   for (size_t i = 0; i < v.size(); ++i)
-      ASSER_EQ(v[i], r[i]);
-}
+//    std::vector<int> v{0, 2, 3, 4};
+//    auto r = gr.shortest(0, 4);
+//    for (size_t i = 0; i < v.size(); ++i)
+//       ASSER_EQ(v[i], r[i]);
+// }
 
 // ---------------------- ~TESTS
 
